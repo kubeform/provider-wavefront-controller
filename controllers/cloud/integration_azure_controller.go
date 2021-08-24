f4f4462e7918ffcd88dd5ce6c1a0fb7babb7afa3
lgtm <1gtm@users.noreply.github.com>
2021-08-24 13:19:39 -0700

    Generate code for provider@v0.0.0-20210610195536-769fabacff12 gen@b27b93c (#8)
    Signed-off-by: 1gtm <1gtm@appscode.com>

--- controllers/cloud/integration_azure_controller.go
+++ controllers/cloud/integration_azure_controller.go
@@ -20,8 +20,8 @@ package cloud
 
 import (
 	"context"
+	"fmt"
 
-	"github.com/go-errors/errors"
 	"github.com/go-logr/logr"
 	tfschema "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	auditlib "go.bytebuilders.dev/audit/lib"
@@ -118,7 +118,7 @@ func (r *IntegrationAzureReconciler) SensitiveSecretWatch(ctx context.Context) h
 
 		sensSec, ok := o.(*v1.Secret)
 		if !ok {
-			log.Error(errors.Errorf("expected a Secret but go a %T", o), "failed to get secret for IntegrationAzure")
+			log.Error(fmt.Errorf("expected a Secret but go a %T", o), "failed to get secret for IntegrationAzure")
 			return nil
 		}
 
